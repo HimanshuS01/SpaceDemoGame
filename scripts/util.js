1abceec37ec148b2766ed878c3be6f3fa039bfd9
manisha poddar <manisha.pdr@gmail.com>
2017-02-08 20:49:47 -0600

    canvas created

--- scripts/util.js
+++ scripts/util.js
@@ -11,6 +11,48 @@
  * @returns A number in the range [min, max]
  * @type Number
  */
+ function init(){
+     canvas = document.getElementById('mycanvas');
+
+
+     pen = canvas.getContext('2d');
+
+     WIDTH = canvas.width;
+     HEIGHT = canvas.height;
+     GAME_OVER = false;
+
+    pen.fillStyle = "#fcf";
+}
+
+function draw(){
+    //Erase the old screen
+    pen.clearRect(0,0,WIDTH,HEIGHT);
+    pen.fillStyle = "#000";
+    pen.fillText = ("set up");
+    }
+
+function update(){
+}
+
+function render(){
+    draw();
+    update();
+   
+   console.log("In Render");
+    if(GAME_OVER==false){
+        window.requestAnimationFrame(render);
+    }
+    else{
+        startGame();
+    }
+}
+
+function startGame(){
+    init();
+    render();
+}
+startGame();
+
 Number.prototype.clamp = function(min, max) {
   return Math.min(Math.max(this, min), max);
 };
