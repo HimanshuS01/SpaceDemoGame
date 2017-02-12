7f33ade1cbe3f398a8ccf29b30ded3c9b1a861af
manisha poddar <manisha.pdr@gmail.com>
2017-02-11 22:49:10 -0600

    all code added

--- scripts/util.js
+++ scripts/util.js
@@ -13,6 +13,7 @@
  */
  FPS = 30;
  playerBullets = [];
+ enemies = [];
 
  function loadImages(){
  	playerImage = new Image();
@@ -21,6 +22,7 @@
 
     playerImage.src = 'images/player.png';
 
+<<<<<<< Updated upstream
     enemyImage.src = 'images/enemy.png';
 
     bullet.src='images/bullet.png';
@@ -100,6 +102,148 @@ document.addEventListener('keydown',function(e){
    player.x = player.x.clamp(0, WIDTH - player.w);
 
 });
+=======
+ 	playerImage.src = 'images/player.png';
+
+ 	enemyImage.src = 'images/enemy.png';
+
+ }
+
+ function init(){
+
+ 	canvas = document.getElementById('mycanvas');
+ 	pen = canvas.getContext('2d');
+
+ 	WIDTH = canvas.width;
+ 	HEIGHT = canvas.height;
+ 	GAME_OVER = false;
+ 	console.log("manisha");
+ 	textX = 50;
+ 	textY = 50;
+
+ 	pen.fillStyle = "#fcf";    
+ 	pen.fillText = ("sup Bro!", 50, 50);
+
+ 	player = {
+ 		color: "red",
+ 		x:220,
+ 		y:465,
+ 		w:32,
+ 		h:32,
+ 		speed:5,
+ 		moving:false,
+ 		draw: function(){
+ 			console.log("draw function");
+ 			pen.fillStyle = this.color;
+ 			pen.fillRect(this.x, this.y, this.w, this.h);
+ 		},
+ 		shoot:function(){
+ 			console.log("phew phew!!");
+ 			var bulletPosition = this.midpoint();
+ 			playerBullets.push(Bullet({
+ 				speed: 5,
+ 				x: bulletPosition.x,
+ 				y: bulletPosition.y,
+
+ 			})) ;
+
+ 		},
+ 		midpoint: function(){
+ 			return {
+ 				x: this.x+this.w/2,
+ 				y: this.y+this.h/2,
+ 			};
+ 		}
+ 		
+ 	};
+ 	document.addEventListener('keydown',function(e){
+ 		if(e.key==" "){
+ 			console.log("space pressed")
+ 			player.shoot();
+ 		}
+        // if(e.key=="ArrowLeft"){
+        //     pokemon.x -= pokemon.speed;
+        // }
+        if(e.key=="ArrowRight"){
+        	player.x += player.speed;
+
+        }
+        if(e.key=="ArrowLeft"){
+        	player.x -= player.speed;
+        }
+
+        player.x = player.x.clamp(0, WIDTH - player.w);
+    });
+ }
+ function draw(){
+    //Erase the old screen
+    pen.clearRect(0,0,WIDTH,HEIGHT);
+    pen.fillStyle = "#E5B7E5";
+    player.draw();
+        // pen.drawImage(playerImage,player.x,player.y,player.w,player.h);
+    // pen.fillText = ("set up");
+    playerBullets.forEach(function(bullet){
+    	bullet.draw();
+    });
+
+    enemies.forEach(function(enemy){
+    	enemy.draw();
+    });
+}
+
+function update(){
+ // textX += 1;
+ // textY += 1;
+ playerBullets.forEach(function(bullet){
+ 	bullet.update();
+ });
+ 
+ playerBullets = playerBullets.filter(function(bullet){
+ 	return bullet.active;
+ });
+
+ enemies.forEach(function(enemy){
+ 	enemy.update();
+ });
+ 
+ enemies = enemies.filter(function(enemy){
+ 	return enemy.active;
+ });
+ // if(Math.random() <0.1){
+ // 	enemies.push(Enemy());
+ // }
+}
+
+function Enemy(I){
+
+	Console.log("Inside Enemy");
+	I = I || {};
+
+	I.active = true;
+	I.age = Math.floor(Math.random()*128);
+	I.color = "#A2B";
+	I.x = WIDTH/4 + Math.random() * WIDTH/2;
+	I.y = 0;
+	I.xVelocity = 0;
+	I.yVelocity = 2;
+	I.width = 32;
+	I.height = 32;
+	I.inBounds = function(){
+		return I.x >=0 && I.x <= WIDTH && I.y >=0 && I.y <= HEIGHT;
+	};
+	I.draw = function(){
+		pen.fillStyle = this.color;
+		pen.fillRect = (this.x, this.y, this.width, this.height);
+	};
+	I.update = function(){
+		I.x += I.xVelocity;
+		I.y += I.yVelocity;
+		I.xVelocity = 3*Math.sin(I.age ^ Math.PI/64);
+		I.age++;
+		I.active = I.active && I.inBounds();
+	};
+
+>>>>>>> Stashed changes
 }
 
 function Bullet(I){
@@ -141,10 +285,18 @@ function update(){
 });
 
 }
+<<<<<<< Updated upstream
 // myVar = setInterval(function(){
 // 	console.log("jhfdshd");
 // 	update();
 // 	draw();
+=======
+
+myVar = setInterval(function(){
+	console.log("jhfdshd");
+	update();
+	draw();
+>>>>>>> Stashed changes
 
 // }, 1000/FPS);
 
