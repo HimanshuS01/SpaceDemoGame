9b830907dc9313b1aaacf4771395eb97fa424ece
manisha poddar <manisha.pdr@gmail.com>
2017-02-15 17:59:22 -0600

    Bullets Issue

--- scripts/util.js
+++ scripts/util.js
@@ -52,57 +52,66 @@ function init(){
    h:32,
    speed:5,
    moving:false,
+   bulletsArray:[],
 
    draw: function(){
     pen.fillStyle = this.color;
         // pen.fillRect(0,0,100,100);
         pen.drawImage(playerImage,player.x,player.y,player.w,player.h);
-        
+        pen.drawImage(bulletImage,150,50);
+
       },
 
       shoot:function(){
         console.log("shooting");
+        Sound.play("shoot");
         bulletposition=this.midpoint();
-        console.log(bulletposition + " Manisha");
+        console.log(" Manisha " + bulletposition.x + " " + bulletposition.y);
+        pen.drawImage(bulletImage,160,50);
         playerBullets.push(Bullet({
-          speed:5,
+          speed:2,
           x: bulletposition.x,
           y: bulletposition.y,
-
-        }));
-        console.log(playerBullets);
+        // console.log(x + " " + y);
+      }));
+        console.log("here"+playerBullets);
+        
       },	
 
       midpoint:function(){
         return{
-          x: this.x + WIDTH/2,
-          y: this.y + HEIGHT/2
+          x: this.x + this.w/2,
+          y: this.y + this.h/2,
         };
+        // return{
+        //   x: this.x + WIDTH/2,
+        //   y: this.y + HEIGHT/2,
+        // };
       },
 
       explode: function(){
         this.active = false;
-      }
+      },
+      
 
     };
 
-    document.addEventListener('keydown',function(e){
+ document.addEventListener('keydown',function(e){
 
-      if(e.key==" "){
-        console.log("space pressed")
-        player.shoot();
-      }
-      if(e.key=="ArrowRight"){
-       player.x += player.speed;
+   if(e.key==" "){
+    console.log("space pressed");
+    player.shoot();
+    }
+     if(e.key=="ArrowRight"){
+     player.x += 2;
 
-     }
-     if(e.key=="ArrowLeft"){
-       player.x -= player.speed;
-     }
+   }
+   if(e.key=="ArrowLeft"){
+     player.x -= 2;
+   }
 
-     player.x = player.x.clamp(0, WIDTH - player.w);
-
-   });
+   player.x = player.x.clamp(0, WIDTH - player.w);
+  });
 
   };
 
@@ -111,8 +120,8 @@ function init(){
     pen.clearRect(0,0,WIDTH,HEIGHT);
     pen.fillStyle = "#E5B7E5";
     player.draw();
-        // pen.drawImage(playerImage,player.x,player.y,player.w,player.h);
-    // pen.fillText = ("set up");
+    // pen.drawImage(playerImage,player.x,player.y,player.w,player.h);
+    // pen.fillText = ("sup bro", textX, textY);
     playerBullets.forEach(function(bullet){
     	bullet.draw();
     });
@@ -123,34 +132,35 @@ function init(){
   }
 
   function update(){
- // textX += 1;
- // textY += 1;
- playerBullets.forEach(function(bullet){
- 	bullet.update();
- });
- 
- playerBullets = playerBullets.filter(function(bullet){
- 	return bullet.active;
- });
+   textX += 1;
+   textY += 1;
 
- enemies.forEach(function(enemy){
- 	enemy.update();
- });
- 
- enemies = enemies.filter(function(enemy){
- 	return enemy.active;
- });
- // if(Math.random() <0.1){
- // 	enemies.push(Enemy());
- // }
-}
+    playerBullets.forEach(function(bullet){
+      bullet.update();
+    });
+
+    playerBullets = playerBullets.filter(function(bullet){
+      return bullet.active;
+    });
+
+    enemies.forEach(function(enemy){
+      enemy.update();
+    });
+
+    enemies = enemies.filter(function(enemy){
+      return enemy.active;
+    });
+    if(Math.random() <1.2){
+      enemies.push(Enemy());
+    }
+  }
 
 
 
 
-function Enemy(I){
+  function Enemy(I){
 
-	Console.log("Inside Enemy");
+	//console.log("Inside Enemy");
 	I = I || {};
 
 	I.active = true;
@@ -166,8 +176,8 @@ function Enemy(I){
 		return I.x >=0 && I.x <= WIDTH && I.y >=0 && I.y <= HEIGHT;
 	};
 	I.draw = function(){
-		pen.fillStyle = this.color;
-		pen.drawImage(enemyImage,20,40, this.width, this.height);
+		pen.fillStyle = "#A2F";
+		pen.drawImage(enemyImage,20,40, I.width, I.height);
 	};
 	I.update = function(){
 		I.x += I.xVelocity;
@@ -177,11 +187,12 @@ function Enemy(I){
 		I.active = I.active && I.inBounds();
 	};
 	I.explode = function(){
-		this.active = false;
-	};
- return I;
-
+    Sound.play("explode");
+    this.active = false;
+  };
+  return I;
 }
+
 
 function Bullet(I){
 	I.active = true;
@@ -189,24 +200,25 @@ function Bullet(I){
 	I.yVelocity = -I.speed;
 	I.width = 3;
 	I.height = 3; 
-	I.color = "#000";
+	I.color = "#ff0";
 
 	I.inBounds = function(){
 		return I.x >=0 && I.x <= WIDTH && I.y >=0 && I.y <= HEIGHT;
 	};
 
-	I.draw = function(){
+ I.draw = function(){
 		// pen.fillStyle = this.color;
-    pen.fillStyle = "#000";
-    pen.drawImage(bulletImage,220,465 , this.width, this.height);
+    pen.fillStyle = "#0cf";
+    // pen.drawImage(bulletImage,220, 400, 32, 32 );
+    pen.fillRect(220,465,I.width, I.height);
   };
 
   I.update = function(){
     I.x += I.xVelocity;
     I.y += I.yVelocity;
+    console.log(I.x +" "+I.y);
     I.active = I.active && I.inBounds();
   };
-
   return I;
 }
 
@@ -225,42 +237,42 @@ function render(){
   draw();
   update();
 
-    console.log("In Render");
-    if(GAME_OVER==false){
-      window.requestAnimationFrame(render);
-    }
-    else{
-      startGame();
-    }
+  //console.log("In Render");
+  if(GAME_OVER==false){
+    window.requestAnimationFrame(render);
   }
+  else{
+    startGame();
+  }
+}
 
-  function collides (a,b){
-   return a.x < b.x + b.width &&
-   a.x + a.width > b.x &&
-   a.y < b.y + b.height &&
-   a.y + a.height > b.y;
+function collides (a,b){
+ return a.x < b.x + b.width &&
+ a.x + a.width > b.x &&
+ a.y < b.y + b.height &&
+ a.y + a.height > b.y;
+}
+
+function handleCollisions(){
+ playerBullets.forEach(function(bullet){
+  enemies.forEach(function(enemy){
+   if (collides(bullet, enemy)){
+    enemy.explode();
+    bullet.active = false;
+  }
+});
+});
+ enemies.forEach(function(enemy){
+  if(collides(enemy,player)){
+   enemy.explode();
+   player.explode();
  }
+});
 
- function handleCollisions(){
-   playerBullets.forEach(function(bullet){
-    enemies.forEach(function(enemy){
-     if (collides(bullet, enemy)){
-      enemy.explode();
-      bullet.active = false;
-    }
-  });
-  });
-   enemies.forEach(function(enemy){
-    if(collides(enemy,player)){
-     enemy.explode();
-     player.explode();
-   }
- });
+}
 
- }
-
- loadImages();
- function startGame(){
+loadImages();
+function startGame(){
   console.log("In the startGame function");
   init();
   render();
